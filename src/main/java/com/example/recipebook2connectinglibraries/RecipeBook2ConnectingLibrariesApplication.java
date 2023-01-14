0ea9cf5ed1c944bd2a8a4fa5eb272c5f0ffbfb53
Aleksandr Deev <geshtait@mail.ru>
2023-01-14 21:23:06 +0500

    Initial commit

---
+++ src/main/java/com/example/recipebook2connectinglibraries/RecipeBook2ConnectingLibrariesApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipebook2connectinglibraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBook2ConnectingLibrariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeBook2ConnectingLibrariesApplication.class, args);
+    }
+
+}
